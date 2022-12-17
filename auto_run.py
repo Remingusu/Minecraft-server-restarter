e2ed41eb669a74697a10d0d31826fb607de5f547
Remingusu <94198201+Remingusu@users.noreply.github.com>
2022-12-17 16:12:39 +0100

    Update auto_run.py

--- auto_run.py
+++ auto_run.py
@@ -6,9 +6,12 @@ os.system('{command}')
 loop = True
 while loop:
     time.sleep(2.5)
+
     with open(os.path.abspath('logs/latest.log'), 'r') as file:
         line = file.readlines()[-1]
+
     if line.find('{text_line}') == '{number}':
         print("Restarting the server")
         os.system('{command}')
-print("Complete server shutdown")
+
+print("Complete server shutdown")*
