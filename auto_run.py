fe9d5fb5bedc2732606c05b978351675a637b3f7
Remingusu <94198201+Remingusu@users.noreply.github.com>
2022-12-17 15:27:36 +0100

    Config

--- auto_run.py
+++ auto_run.py
@@ -8,7 +8,7 @@ while loop:
     time.sleep(2.5)
     with open(os.path.abspath('logs/latest.log'), 'r') as file:
         line = file.readlines()[-1]
-    if line.find('Server shut down correctly, ending gracefully') == 79:
+    if line.find('{text_line}') == '{number}':
         print("Restarting the server")
         os.system('{command}')
 print("Arrêt complet du serveur")
